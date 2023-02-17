0247d4a64fdaf19a62362ec6f936d5f4c9533b5f
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-02-17 13:01:52 -0800

    added to Project2

---
+++ Project2_Functions/JS/main.js
@@ -0,0 +1,6 @@
+function Concatenating() { //when function is called prints sentence that is concatenated.//
+    var sentence = "This is the first part"; //First variable in concatenation//
+    sentence += " and this is the last part"; //Second part of concatenation//
+    document.getElementById("Concatenate").innerHTML = sentence; 
+}
+
