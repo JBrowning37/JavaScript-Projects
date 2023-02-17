b9195ece42dea9e24c3a84ca6e634bb5a487fede
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-02-17 11:46:18 -0800

    Adding JS to index

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+window.alert("Hello!") //Creates an alert that says hello//
+
+document.write("Hope you\'re having a great day!") //Writes the initial greeting//
+
+var Sent1 = "This is the beginning of the string" //Variable one to be concatenated//
+
+var Sent2 = " and this is the end of the string" //Variable two to be concatenated//
+
+document.write(Sent1 + Sent2) //Concatenation of Variables Sent1 and Sent2//
+
+document.write(3+3) //Writes the expressions and executes//
+
