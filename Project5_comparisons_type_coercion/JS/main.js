e2f0167dc94b01e6cbcdccf400ba46f9b0799e44
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-02-19 18:35:07 -0800

    Created project 5 and added html and js

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,19 @@
+document.write(typeof "hello"); // outputs what type "" is //
+
+document.write("2" + 2); // because they are different types adds 2 onto the end of "2"//
+
+x = 2;
+y = 2;
+document.write(x === y); //compares x and y for equality//
+
+document.write(2 == 2); // checking for equality // 
+
+document.write(2>1); //checks if 2 is greater than 1//
+
+document.write(2<3); //checks if 2 is less than 3//
+
+document.write(2<4 && 2<3); //checks of both operations are true or false//
+
+document.write(2<3 || 2>4); //checks if either operations are true//
+
+document.write(!2<3); //checks if the operation is NOT true or false (double negative)//
